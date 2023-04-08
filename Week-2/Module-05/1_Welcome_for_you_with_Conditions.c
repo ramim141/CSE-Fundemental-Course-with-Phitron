93d0c9675da0d03badb71c7a68d90cf87e78dda2
Ramim Ahmed <125358464+ramim141@users.noreply.github.com>
2023-04-08 22:59:50 +0600

    Add files via upload

---
+++ Week-2/Module-05/1_Welcome_for_you_with_Conditions.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int A, B;
+    printf("Enter the value of A:" );
+    scanf("%d", &A);
+
+    printf("\nEnter the value of B: ");
+    scanf("%d", &A);
+
+    if(A>=B)
+    {
+        printf("Yes");
+    }
+    else 
+    {
+        printf("No");
+    }
+
+
+    return 0; 
+}
