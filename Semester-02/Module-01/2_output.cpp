9206b8e7f0ec286c1f9ded1a5f8d384dd1c50dba
Ramim Ahmed <125358464+ramim141@users.noreply.github.com>
2023-05-23 18:15:24 +0600

    Add files via upload

---
+++ Semester-02/Module-01/2_output.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //printf("Hello World"); ---> for c
+    std:: cout<<"Hello World";
+    std:: cout<<"HELLO\n";
+    std:: cout<<"WORLD\n";
+    std:: cout<<"Hello "<<"World"<<endl;
+    std:: cout<<"Hello" << endl<<"World"<< endl;
+
+    int a=100, b=200;
+    //printf("%d", a);
+    std:: cout<<a<<" "<<b<< endl;
+    cout<<a<<" "<<b<<endl;
+
+
+    return 0; 
+}
+
---
+++ Semester-02/Module-01/4_float_output.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    float a;
+    cout<<"Enter The valure of a : ";
+    cin>>a;
+    cout<<fixed<<setprecision(3)<<a;
+    return 0; 
+}
